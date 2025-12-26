be024c0703ec23f079f56dc16ca43cac0093e19e
vinaysahu <sahuvinay742@gmail.com>
2025-12-26 13:30:58 +0530

    changes

--- customers/admin/CustomerEnquiryAdmin.py
+++ customers/admin/CustomerEnquiryAdmin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
 from ..models.CustomerEnquiry import CustomerEnquiry
-from django.utils.html import format_html
-from django.contrib.auth.hashers import make_password
-import secrets
       
 class CustomerEnquiryAdmin(admin.ModelAdmin):
 
-    list_display = [ "name", "project_id", "phone_no", "email", "requirement", "budget_min", "notes", "follow_up_date", "notes", "status", "created_at", "updated_at" ] # grid mae kaisa view
-    exclude = ('created_at', 'updated_at')       # Remove from FORM
+    list_display_superuser = [ "name", "project_id", "phone_no", "email", "requirement", "budget_min", "notes", "follow_up_date", "notes", "status", "created_at", "updated_at" ]
+    exclude_superuser = ('created_at', 'updated_at')
+
+    list_display_staff = [ "name", "phone_no", "email", "requirement", "budget_min", "notes", "follow_up_date", "notes", "status", "created_at", "updated_at" ]
+    exclude_staff = ('created_by', 'created_at', 'updated_at', 'project_id')
+
+    def get_list_display(self, request):
+        if request.user.is_superuser:
+            return self.list_display_superuser
+        return self.list_display_staff
+
+    # 🔹 ROLE BASED EXCLUDE (FORM)
+    def get_exclude(self, request, obj=None):
+        if request.user.is_superuser:
+            return self.exclude_superuser
+        return self.exclude_staff
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
--- customers/admin/PropertySellRequestAdmin.py
+++ customers/admin/PropertySellRequestAdmin.py
@@ -3,8 +3,22 @@ from ..models.PropertySellRequest import PropertySellRequest
       
 class PropertySellRequestAdmin(admin.ModelAdmin):
 
-    list_display = [ "owner_name", "phone_no", "alt_phone_no", "email", "address", "area_sqyd", "dimension", "expected_price", "reason_for_selling", "notes", "status", "created_at", "updated_at" ] # grid mae kaisa view
-    exclude = ('created_at', 'updated_at')       # Remove from FORM
+    list_display_superuser = [ "owner_name", "phone_no", "alt_phone_no", "project", "email", "address", "area_sqyd", "dimension", "expected_price", "reason_for_selling", "notes", "status", "created_at", "updated_at" ] # grid mae kaisa view
+    exclude_superuser = ('created_at', 'updated_at')       # Remove from FORM
+
+    list_display_staff = [ "owner_name", "phone_no", "alt_phone_no", "email", "address", "area_sqyd", "dimension", "expected_price", "reason_for_selling", "notes", "status", "created_at", "updated_at" ]
+    exclude_staff = ('created_by', 'created_at', 'updated_at', "project") 
+
+    def get_list_display(self, request):
+        if request.user.is_superuser:
+            return self.list_display_superuser
+        return self.list_display_staff
+
+    # 🔹 ROLE BASED EXCLUDE (FORM)
+    def get_exclude(self, request, obj=None):
+        if request.user.is_superuser:
+            return self.exclude_superuser
+        return self.exclude_staff
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
