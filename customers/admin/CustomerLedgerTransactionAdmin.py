bc6595bb76da0427a6ca653ea4e17e064a6f1e74
vinaysahu <sahuvinay742@gmail.com>
2026-04-05 15:53:12 +0530

    channges

--- customers/admin/CustomerLedgerTransactionAdmin.py
+++ customers/admin/CustomerLedgerTransactionAdmin.py
@@ -3,12 +3,25 @@ from django.contrib import admin
 from ..models.Customers import Customers
 from ..models.CustomerLedgerTransaction import CustomerLedgerTransaction
 from ..forms.CustomerLedgerTransactionForm import CustomerLedgerTransactionForm
-from common.filters.adminModelFilter import customDropdownFilterForAnotherTable
-from django.db.models import Q
-from django.contrib.auth.models import User
-from projects.models.Projects import Projects
-from django.db.models import F
 from common.utils.format_currency import format_indian_currency
+from django.db.models import Sum
+from django.db.models.functions import Coalesce
+
+class CustomerFilter(admin.SimpleListFilter):
+    title = 'Customer'
+    parameter_name = 'customer'
+
+    def lookups(self, request, model_admin):
+        customers = Customers.objects.all()
+        return [
+            (customer.id, f"{customer.first_name} {customer.last_name}")
+            for customer in customers
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(customer_ledger__customer_id__id=self.value())
+        return queryset
       
 class CustomerLedgerTransactionAdmin(admin.ModelAdmin):
 
@@ -17,12 +30,35 @@ class CustomerLedgerTransactionAdmin(admin.ModelAdmin):
     list_display = ['paid_on', 'paid_to', 'payment_type','formatted_amount','detail' ] # grid mae kaisa view
     exclude = ('created_at', 'updated_at')
 
-    list_filter = [customDropdownFilterForAnotherTable("Customers", "customers", "username", Customers, ["customer_ledger__customer_id"]  ), 'paid_to']
+    # list_filter = [customDropdownFilterForAnotherTable("Customer Ledger", "Customer Ledger", "customer_id", CustomerLedger, ["customer_ledger__customer_id"]  ), 'paid_to']
+    list_filter = [CustomerFilter, 'paid_to']
 
-    
+    search_fields = [
+        'customer_ledger__customer_id__first_name',
+        'customer_ledger__customer_id__last_name',
+        'detail'
+    ]
+
     def formatted_amount(self, obj):
         return format_indian_currency(obj.amount)
     formatted_amount.short_description = "Amount"
+
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(request, extra_context=extra_context)
+
+        try:
+            queryset = response.context_data['cl'].queryset
+
+            total_amount = queryset.aggregate(
+                total=Sum('amount')
+            )['total']
+
+            response.context_data['total_amount'] = format_indian_currency(total_amount)
+
+        except (AttributeError, KeyError):
+            pass
+
+        return response
 
     def has_add_permission(self, request, obj=None):
         if request.user.is_superuser:
@@ -41,19 +77,5 @@ class CustomerLedgerTransactionAdmin(admin.ModelAdmin):
 
     class Media:
             js = ('admin/js/amountFormat1.js',)
-
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        qs = qs.order_by(F('paid_on').asc(nulls_last=True))
-        user = request.user
-
-        if user.is_superuser:
-            return qs
-
-        return qs.filter(
-            Q(user_ledger__creditor=user) | Q(user_ledger__debtor=user)
-        ).distinct()
-    
-
 
 admin.site.register(CustomerLedgerTransaction,CustomerLedgerTransactionAdmin)
